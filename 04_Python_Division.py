66b7bbb49fb75c826c5423a3e2c3388c987985b0
ramesh6859 <ramesh6859@gmail.com>
2021-08-24 12:20:46 +0530

    Fourth Program

--- 04_Python_Division.py
+++ 04_Python_Division.py
@@ -3,18 +3,19 @@ Program		:	"Python: Division"
 Author		:	Ramesh K P
 Language    :	Python
 IDE		    :	Visual Studio
-Date		:	23/08/2021
+Date		:	24/08/2021
 mail		:	rameshofficial75@gmail.com
-Expected Input	:	First Line : 
-                    Second Line : 
-                    Third Line : Input a sentence
-Expected Output	:	First Line : Display the input character
-			Second Line : Display the input word
-			Third Line : Display the input sentence
+Expected Input	:	First Line : Input the first integer
+                    Second Line : Inout the second integer
+Expected Output	:	First Line : Display the integer division of two numbers
+                    Second Line : Display the float division of two numbers
 """
 
+from __future__ import division
 if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(int(a/b))
-    print(a/b)
+    print("\n")
+    first_number = int(input("Enter the first number: "))
+    second_number = int(input("Enter the second number: "))
+    print("Integer division of two numbers: " + str(first_number//second_number))
+    print("Float division of two numbers: " + str(first_number/second_number))
+    print("\n")
